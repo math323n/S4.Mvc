f2e13e39e643b21da369b0a98198bb4c7fd4b1cd
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-30 10:43:41 +0200

    Added analog clock to index.

--- S4.Mvc.Web/Controllers/HomeController.cs
+++ S4.Mvc.Web/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 using S4.Mvc.Web.Models;
 
 namespace S4.Mvc.Web.Controllers
