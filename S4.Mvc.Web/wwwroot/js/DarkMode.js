fe7ba2c4ee958eb9775d693f2ddc3b6464c187ea
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-30 14:42:50 +0200

    Changed stuff

---
+++ S4.Mvc.Web/wwwroot/js/DarkMode.js
@@ -0,0 +1,9 @@
+﻿const TABLE = document.querySelector("#productTable");
+const DARK = document.querySelector("#dark");
+
+function TableDarkMode() {
+    TABLE.classList.add(".table-dark");
+}
+
+DARK.addEventListener("click", TableDarkMode(), false);
+
