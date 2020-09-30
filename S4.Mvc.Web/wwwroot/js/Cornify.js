6f5d5d79b228184309bf63b30e98e89f75e69980
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-30 10:52:05 +0200

    Added cornify script Enter code autist in browser

---
+++ S4.Mvc.Web/wwwroot/js/Cornify.js
@@ -0,0 +1,13 @@
+﻿const pressed = [];
+const secretCode = 'autist';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        console.log('DING DING!');
+        cornify_add();
+    }
+    console.log(pressed);
+});
