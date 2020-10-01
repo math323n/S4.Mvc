43bc29a3c3e0bf38d77266b5cdc687bd036e3fdd
Troll Coder <math323n@edu.campusvejle.dk>
2020-10-01 13:57:29 +0200

    Did some edits

--- S4.Mvc.Web/wwwroot/js/Cornify.js
+++ S4.Mvc.Web/wwwroot/js/Cornify.js
@@ -1,19 +1,15 @@
 ﻿const pressed = [];
-const secretCode = 'autist';
+const secretCode = 'pony';
 
-var button = document.querySelector('cornify');
 
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     if (pressed.join('').includes(secretCode)) {
-        console.log('DING DING!');
+        console.log('Congratulations, you won.');
         cornify_add();
     }
     console.log(pressed);
 });
 
-button.addEventListener('click', cornify_add(), false)
-
-document.addEventListener('DOMContentLoaded', cornify_add(), false);
